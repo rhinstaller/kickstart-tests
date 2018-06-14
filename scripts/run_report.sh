91514c3873a6f05032032b41f3ed4fdacbe5fecd
Martin Kolman <mkolman@redhat.com>
2018-06-14 16:15:22 +0200

    Fix the run_reports.sh script
    A couple things change and the report generation script
    needs to be adjusted accordingly:
    
    - handle new formatting of the RESULT line
    - handle longer test names
    - handle new timeout error message

--- scripts/run_report.sh
+++ scripts/run_report.sh
@@ -1,42 +1,50 @@
 #!/bin/gawk -f
 
 BEGIN {
-    printf("\n\n\n%-40s | %-10s | %s\n", "TEST", "RESULT", "EXPLANATION");
+    printf("\n\n\n%-55s | %-10s | %s\n", "TEST", "RESULT", "EXPLANATION");
     printf("-------------------------------+------------+--------------------------------------------------------\n");
     FS=":";
     count=0;
     failed=0;
     timed_out=0;
 }
-/^RESULT:/ { if ($4 == "Test timed out") {
+/: RESULT:/ { if ($4 == "Test timed out") {
                  result = "TIMED OUT";
                  explanation = "";
                  timed_out++;
              } else if (match($0, "Traceback")) {
                  result = "FAILED";
-                 explanation = "Traceback";
+                 explanation = "Traceback: " $10;
+                 failed++;
+             } else if (match($0, "traceback")) {
+                 result = "FAILED";
+                 explanation = "Traceback.";
+                 failed++;
+             } else if (match($0, "failed on line")) {
+                 result = "FAILED";
+                 explanation = "error in log: "$11 $12 $13
                  failed++;
              } else if (match($0, "SUCCESS")) {
-                 result = $3;
+                 result = $6;
                  explanation = "";
              } else {
-                 result = $3;
-                 explanation = substr($0, index($0, $4), 55);
+                 result = $6;
+                 explanation = $7
                  failed++;
              }
 
-             printf("%-40s | %-10s | %s\n", $2, result, explanation);
+             printf("%-55s | %-10s | %s\n", $5, result, explanation);
              count++
            }
 END {
     printf("\n");
-    printf("============================================================================\n");
+    printf("=====================================================================================================\n");
     printf("Test suite for kickstart tests summary:\n");
-    printf("============================================================================\n");
+    printf("=====================================================================================================\n");
     printf("# TOTAL:      %s\n", count);
     printf("# PASS:       %s\n", count - failed - timed_out);
     printf("# FAILED:     %s\n", failed);
     printf("# TIMED OUT:  %s\n", timed_out);
-    printf("============================================================================\n");
+    printf("=====================================================================================================\n");
     printf("\n\n");
 }

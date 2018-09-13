1c31a7ea42998d3f9ed455d27b46fe4cfc880f5f
Martin Kolman <mkolman@redhat.com>
2018-09-13 18:31:04 +0200

    Add host id for kickstart test result line
    When many kickstart tests are running in parallel on different
    machines/VM hosts it can be very useful to know on which host
    a given test was run.
    
    This information can be used for example to debug host specific issues
    that break tests (machine is too slow or unstable, has broken
    networking, etc.) or to directly access test logs on machines during a
    long run.
    
    So add a host id (at the moment simply the machine hostname) to
    the RESULT line, so we can easily see on which machine did the test run.
    
    Also adjust the run_report.sh script to be compatible with the new RESULT format.

--- scripts/run_report.sh
+++ scripts/run_report.sh
@@ -1,20 +1,20 @@
 #!/bin/gawk -f
 
 BEGIN {
-    printf("\n\n\n%-55s | %-10s | %s\n", "TEST", "RESULT", "EXPLANATION");
-    printf("-------------------------------+------------+--------------------------------------------------------\n");
+    printf("\n\n\n%-55s | %-15s | %-10s | %s\n", "TEST", "HOST", "RESULT", "EXPLANATION");
+    printf("--------------------------------------------------------+-----------------+------------+-------------------------------------------\n");
     FS=":";
     count=0;
     failed=0;
     timed_out=0;
 }
-/: RESULT:/ { if ($7 == "Test timed out") {
+/: RESULT:/ { if ($8 == "Test timed out") {
                  result = "TIMED OUT";
                  explanation = "";
                  timed_out++;
              } else if (match($0, "Traceback")) {
                  result = "FAILED";
-                 explanation = "Traceback: " $10;
+                 explanation = "Traceback: " $11;
                  failed++;
              } else if (match($0, "traceback")) {
                  result = "FAILED";
@@ -22,29 +22,29 @@ BEGIN {
                  failed++;
              } else if (match($0, "failed on line")) {
                  result = "FAILED";
-                 explanation = "error in log: "$11 $12 $13
+                 explanation = "error in log: "$12 ":" $13 ":" $14
                  failed++;
              } else if (match($0, "SUCCESS")) {
-                 result = $6;
+                 result = $7;
                  explanation = "";
              } else {
-                 result = $6;
-                 explanation = $7
+                 result = $7;
+                 explanation = $8
                  failed++;
              }
 
-             printf("%-55s | %-10s | %s\n", $5, result, explanation);
+             printf("%-55s | %-15s | %-10s | %s\n", $5, $6, result, explanation);
              count++
            }
 END {
     printf("\n");
-    printf("=====================================================================================================\n");
+    printf("===================================================================================================================================\n");
     printf("Test suite for kickstart tests summary:\n");
-    printf("=====================================================================================================\n");
+    printf("===================================================================================================================================\n");
     printf("# TOTAL:      %s\n", count);
     printf("# PASS:       %s\n", count - failed - timed_out);
     printf("# FAILED:     %s\n", failed);
     printf("# TIMED OUT:  %s\n", timed_out);
-    printf("=====================================================================================================\n");
+    printf("===================================================================================================================================\n");
     printf("\n\n");
 }

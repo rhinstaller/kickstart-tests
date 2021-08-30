9c49dd691fa73b2eace5ef8e54c5525c37c58f44
Radek Vykydal <rvykydal@redhat.com>
2021-08-30 10:49:44 +0200

    Disable network-options-pre test temporarily on rhel-8 (#1990145)
    Disable network-options-pre test on rhel-8 until rhbz#1990145 is fixed.

--- network-options-pre.sh
+++ network-options-pre.sh
@@ -24,7 +24,7 @@
 # injected in initrd) network devices are not found in sysfs in the time of
 # parsing the kickstart.
 
-TESTTYPE=${TESTTYPE:-"network"}
+TESTTYPE=${TESTTYPE:-"network rhbz1990145"}
 
 . ${KSTESTDIR}/functions.sh
 

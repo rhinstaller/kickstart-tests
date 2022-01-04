25f6b013405ef7ecbc482bc89bcd6d17fcc6e866
Radek Vykydal <rvykydal@redhat.com>
2022-01-04 10:11:27 +0100

    Add scenario for running daily tests in Permian

---
+++ containers/runner/fetch_daily_iso.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eux
+
+DAILY_ISO_TOKEN=$1
+OUTPUT_PATH=${2:-"boot.iso"}
+
+CURL="curl -u token:$(cat $DAILY_ISO_TOKEN) --show-error --fail"
+RESPONSE=$($CURL --silent https://api.github.com/repos/rhinstaller/kickstart-tests/actions/artifacts)
+ZIP=$(echo "$RESPONSE" | jq --raw-output '.artifacts | map(select(.name == "images"))[0].archive_download_url')
+echo "INFO: Downloading $ZIP ..."
+$CURL -L -o images.zip "$ZIP"
+unzip images.zip
+mv boot.iso ${OUTPUT_PATH}

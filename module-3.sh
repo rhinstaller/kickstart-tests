a348e36a7d635b9fbaf2b63625b2a2a63db151cb
Radek Vykydal <rvykydal@redhat.com>
2022-02-11 08:52:01 +0100

    Disable module-3 test temporarily on daily-iso (gh#658)
    Disable module-3 test on daily-iso until gh#658 is fixed.

--- module-3.sh
+++ module-3.sh
@@ -17,6 +17,6 @@
 #
 # Red Hat Author(s): Martin Kolman <mkolman@redhat.com>
 
-TESTTYPE="packaging modularity skip-on-rhel"
+TESTTYPE="packaging modularity skip-on-rhel gh658"
 
 . ${KSTESTDIR}/functions.sh

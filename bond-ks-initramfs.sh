0a162000265ab499b09b59f99c67f8189d0aa432
Radek Vykydal <rvykydal@redhat.com>
2022-10-25 15:00:51 +0200

    Disable bond-ks-initramfs on rhel9 temporarrily (rhbz#2133053)
    Disable bond-ks-initramfs on rhel9 until rhbz#2133053 is fixed.

--- bond-ks-initramfs.sh
+++ bond-ks-initramfs.sh
@@ -17,7 +17,7 @@
 #
 # Red Hat Author(s): Radek Vykydal <rvykydal@redhat.com>
 
-TESTTYPE=${TESTTYPE:-"network rhbz1940919"}
+TESTTYPE=${TESTTYPE:-"network rhbz1940919 rhbz2133053"}
 
 . ${KSTESTDIR}/functions.sh
 

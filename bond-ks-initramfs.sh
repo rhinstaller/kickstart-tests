f44ab1ef9ae7c60f7502a5042cc5584df052c68c
Radek Vykydal <rvykydal@redhat.com>
2022-11-29 12:46:16 +0100

    Enable bond-ks-initramfs test on rhel8 and rhel9

--- bond-ks-initramfs.sh
+++ bond-ks-initramfs.sh
@@ -17,7 +17,7 @@
 #
 # Red Hat Author(s): Radek Vykydal <rvykydal@redhat.com>
 
-TESTTYPE=${TESTTYPE:-"network rhbz1940919 rhbz2133053"}
+TESTTYPE=${TESTTYPE:-"network"}
 
 . ${KSTESTDIR}/functions.sh
 

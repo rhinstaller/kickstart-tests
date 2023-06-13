623d07b6bd2bd325fba4e71936529b3be892938a
Jan Stodola <jstodola@redhat.com>
2023-06-13 15:31:15 +0200

    Enable network-dns-search on RHEL
    This feature is implemented in RHEL-8.9 (bug 1656662) and RHEL-9.3 (bug
    2065754).

--- network-dns-search.sh
+++ network-dns-search.sh
@@ -19,6 +19,6 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="network skip-on-rhel"
+TESTTYPE="network"
 
 . ${KSTESTDIR}/functions.sh

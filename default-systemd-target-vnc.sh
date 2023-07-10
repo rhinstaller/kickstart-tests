b1d1bcf6bfb4aa82e5983c7e89f2478717870e63
Radek Vykydal <rvykydal@redhat.com>
2023-07-10 11:19:22 +0200

    Disable vnc tests temporarily on daily-iso (gh#978)
    Disable vnc tests on daily-iso until gh978 is fixed.

--- default-systemd-target-vnc.sh
+++ default-systemd-target-vnc.sh
@@ -19,7 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="services"
+TESTTYPE="services gh978"
 
 . ${KSTESTDIR}/functions.sh
 . ${KSTESTDIR}/validate-lib-services.sh

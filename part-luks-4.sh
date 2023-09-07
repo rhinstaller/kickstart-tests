961019a4f0fdb5aacf73be3f5cd5249cd3fb6a1d
Radek Vykydal <rvykydal@redhat.com>
2023-09-07 10:35:41 +0200

    Enable part-luks-* tests on fedora.
    Reading of tests results on Fedora seems to work reliably enough.
    On RHEL it is not the case.

--- part-luks-4.sh
+++ part-luks-4.sh
@@ -17,10 +17,11 @@
 #
 # Red Hat Author(s): Vendula Poncova <vponcova@redhat.com>
 
+# On RHEL the test is manual because of broken reading of the results from image.
 # Check the results on the running VM.
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="manual storage partition luks"
+TESTTYPE="skip-on-rhel storage partition luks"
 
 . ${KSTESTDIR}/functions.sh
 

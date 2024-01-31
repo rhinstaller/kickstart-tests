fc036b78b9c9b20b6a6f9a1f68e87082f3dd7695
Radek Vykydal <rvykydal@redhat.com>
2024-01-31 09:35:59 +0100

    Disable *bridge-httpks temporarily on daily-iso (gh#1046)
    Disable *bridge-httpks on daily-iso until gh1046 is fixed.

--- bindtomac-bridge-httpks.sh
+++ bindtomac-bridge-httpks.sh
@@ -19,6 +19,6 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="network"
+TESTTYPE="network gh1046"
 
 . ${KSTESTDIR}/bridge-httpks.sh
--- bridge-httpks.sh
+++ bridge-httpks.sh
@@ -19,7 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE=${TESTTYPE:-"network"}
+TESTTYPE=${TESTTYPE:-"network gh1046"}
 
 . ${KSTESTDIR}/functions.sh
 

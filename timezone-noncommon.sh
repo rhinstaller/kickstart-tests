ac3a0bb0233565b17a313d6321a358ae04a8526a
Radek Vykydal <rvykydal@redhat.com>
2024-02-14 09:03:46 +0100

    Revert "Disable timezone-noncommon temporarily on rhel8 (jiraRHEL-13151)"
    This reverts commit 17204fc50310c5ff19acaf9d67d08df1e0775d12.

--- timezone-noncommon.sh
+++ timezone-noncommon.sh
@@ -19,6 +19,6 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="time jiraRHEL-13151"
+TESTTYPE="time"
 
 . ${KSTESTDIR}/functions.sh

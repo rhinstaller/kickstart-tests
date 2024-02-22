c6bd379a30cb6ab2c17731c8ab794ff326eea870
Vojtech Trefny <vtrefny@redhat.com>
2024-02-22 11:41:38 +0100

    Add a custom validate function for raid-ddf test case
    The default validate uses virt-copy-out which doesn't work with
    RAIDs.

--- raid-ddf.sh
+++ raid-ddf.sh
@@ -39,3 +39,21 @@ additional_runner_args() {
     echo "--wait $(get_timeout)"
 }
 
+validate() {
+    disksdir=$1
+    args=$(for d in ${disksdir}/disk-*img; do echo -a ${d}; done)
+
+    # There should be a /root/RESULT file with results in it.  Check
+    # its contents and decide whether the test finally succeeded or
+    # not.
+    result=$(run_with_timeout ${COPY_FROM_IMAGE_TIMEOUT} "virt-cat ${args} -m /dev/disk/by-label/rootfs /root/RESULT")
+    if [[ $? != 0 ]]; then
+        status=1
+        echo '*** /root/RESULT does not exist in VM image.'
+    elif [[ "${result}" != SUCCESS* ]]; then
+        status=1
+        echo "${result}"
+    fi
+
+    return ${status}
+}

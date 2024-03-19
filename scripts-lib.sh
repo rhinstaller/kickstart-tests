640764860bd9957e96127736a5585fdf0dafd378
Radek Vykydal <rvykydal@redhat.com>
2024-03-19 12:00:01 +0100

    Update repo-install for added rhel10 scenario

--- scripts-lib.sh
+++ scripts-lib.sh
@@ -6,7 +6,20 @@
 function is_rhel8 {
    local os_name=$1
    local os_version=$2
-   if [ ${os_name} == "rhel" -a ${os_version:0:1} == "8" ] ; then
+   if [ ${os_name} == "rhel" ] && [ ${os_version:0:1} == "8" ] ; then
+       echo "yes"
+   else
+       echo "no"
+   fi
+}
+
+# is_rhel9 KSTESTS_OS_NAME KSTESTS_OS_VERSION
+# Are we testing rhel 9 (based on the name and version detected from boot.iso) ?
+# Returns "yes" or "no".
+function is_rhel9 {
+   local os_name=$1
+   local os_version=$2
+   if [ ${os_name} == "rhel" ] && [ ${os_version:0:1} == "9" ] ; then
        echo "yes"
    else
        echo "no"

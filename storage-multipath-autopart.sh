26a2212eefda7adf395f8899c93e47010b7e3a40
Radek Vykydal <rvykydal@redhat.com>
2024-03-25 12:53:24 +0100

    Disable tests failing on newly added rhel10 scenario
    The issues are tracked in:
    
    gh1104
    gh1106
    gh1105
    gh1108
    gh1109
    gh1107
    gh1110

--- storage-multipath-autopart.sh
+++ storage-multipath-autopart.sh
@@ -19,7 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="autopart storage rhbz1853668"
+TESTTYPE="autopart storage rhbz1853668 gh1110"
 
 . ${KSTESTDIR}/functions.sh
 

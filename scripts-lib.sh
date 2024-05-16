8cb666a60b53e3a7b85be36738e16f4bfdb686f3
Radek Vykydal <rvykydal@redhat.com>
2024-05-16 14:35:52 +0200

    Consider centos artifacts as rhel platform
    This should allow us to run centos images with tests for rhel10
    platform.

--- scripts-lib.sh
+++ scripts-lib.sh
@@ -1,23 +1,47 @@
 # Common functions for %post or %pre kickstart section
 
+# get_platform KSTEST_OS_NAME KSTEST_OS_VERSION
+# Get the platform (fragments platform / --platform launch script option).
+# For fedora return only "fedora" prefix (only fedora_rawhide platform is supported currently).
+function get_platform {
+   local os_name=$1
+   local os_version=$2
+   local platform=""
+
+   if [ ${os_name} == "rhel" ] || [ ${os_name} == "centos" ] ; then
+       platform=rhel
+       os_major=$(echo "${os_version}" | grep -oE "^[[:digit:]]+")
+       if [ -n "${os_major}" ] ; then
+           platform="rhel${os_major}"
+       fi
+   fi
+   if [ ${os_name} == "fedora" ] ; then
+       platform=fedora
+   fi
+
+   echo ${platform}
+}
+
+
 # is_rhel8 KSTESTS_OS_NAME KSTESTS_OS_VERSION
 # Are we testing rhel 8 (based on the name and version detected from boot.iso) ?
 function is_rhel8 {
    local os_name=$1
    local os_version=$2
-   if [ ${os_name} == "rhel" ] && [ ${os_version:0:1} == "8" ] ; then
+   if [ "$(get_platform ${os_name} ${os_version})" == "rhel8" ] ; then
        return 0
    else
        return 1
    fi
 }
 
+
 # is_rhel9 KSTESTS_OS_NAME KSTESTS_OS_VERSION
 # Are we testing rhel 9 (based on the name and version detected from boot.iso) ?
 function is_rhel9 {
    local os_name=$1
    local os_version=$2
-   if [ ${os_name} == "rhel" ] && [ ${os_version:0:1} == "9" ] ; then
+   if [ "$(get_platform ${os_name} ${os_version})" == "rhel9" ] ; then
        return 0
    else
        return 1

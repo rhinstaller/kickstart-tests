d5b2933709087e29ec10d81b5ae3cfbb1730c257
Radek Vykydal <rvykydal@redhat.com>
2024-05-17 08:53:23 +0200

    Replace is_rhel8 and is_rhel9 with get_platform function

--- scripts-lib.sh
+++ scripts-lib.sh
@@ -21,29 +21,3 @@ function get_platform {
 
    echo ${platform}
 }
-
-
-# is_rhel8 KSTESTS_OS_NAME KSTESTS_OS_VERSION
-# Are we testing rhel 8 (based on the name and version detected from boot.iso) ?
-function is_rhel8 {
-   local os_name=$1
-   local os_version=$2
-   if [ "$(get_platform ${os_name} ${os_version})" == "rhel8" ] ; then
-       return 0
-   else
-       return 1
-   fi
-}
-
-
-# is_rhel9 KSTESTS_OS_NAME KSTESTS_OS_VERSION
-# Are we testing rhel 9 (based on the name and version detected from boot.iso) ?
-function is_rhel9 {
-   local os_name=$1
-   local os_version=$2
-   if [ "$(get_platform ${os_name} ${os_version})" == "rhel9" ] ; then
-       return 0
-   else
-       return 1
-   fi
-}

8665bc1f5519dfb07a2772465bf671e146bda42d
Radek Vykydal <rvykydal@redhat.com>
2024-08-05 13:34:27 +0200

    Revert "Disable tests using ext2 temporarily on rhel10 (gh#1178)"
    This reverts commit e593174a3395cc5a74c830b4596ce57da0248ee1.

--- autopart-fstype.sh
+++ autopart-fstype.sh
@@ -19,7 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="autopart storage gh1178"
+TESTTYPE="autopart storage"
 
 
 . ${KSTESTDIR}/functions.sh

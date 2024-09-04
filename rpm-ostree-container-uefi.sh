3329371a5be639f6bed1afb6188d68125523db1d
Radek Vykydal <rvykydal@redhat.com>
2024-09-04 08:33:51 +0200

    Disable rpm-ostree-container-uefi until gh1298 is fixed

--- rpm-ostree-container-uefi.sh
+++ rpm-ostree-container-uefi.sh
@@ -18,7 +18,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="payload uefi ostree bootc reboot skip-on-rhel-8 skip-on-rhel-9 skip-on-rhel-10"
+TESTTYPE="payload uefi ostree bootc reboot skip-on-rhel-8 skip-on-rhel-9 skip-on-rhel-10 gh1298"
 
 . ${KSTESTDIR}/functions.sh
 

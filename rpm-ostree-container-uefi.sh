ffac0f47a4be3c2d18f231496f07134a473336b8
Jiri Konecny <jkonecny@redhat.com>
2024-10-21 15:31:33 +0200

    Extend container-uefi with fix for vconsole keymap
    It is fix for
    https://bugzilla.redhat.com/show_bug.cgi?id=1890085
    https://issues.redhat.com/browse/INSTALLER-2537
    
    Currently, virtual console is not correctly set from localed on
    an ostree installations. To resolve this issue let's put the
    vconsole.keymap to the installed system kernel arguments.

--- rpm-ostree-container-uefi.sh
+++ rpm-ostree-container-uefi.sh
@@ -18,7 +18,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="payload uefi ostree bootc reboot skip-on-rhel-8 skip-on-rhel-9 skip-on-rhel-10"
+TESTTYPE="payload uefi ostree bootc keyboard reboot skip-on-rhel-8 skip-on-rhel-9 skip-on-rhel-10"
 
 . ${KSTESTDIR}/functions.sh
 

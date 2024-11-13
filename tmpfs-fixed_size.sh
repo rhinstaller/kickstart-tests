fd2982f4cc9f4211b7afaf70326299b74da98019
Radek Vykydal <rvykydal@redhat.com>
2024-11-13 15:06:11 +0100

    Disable tmpfs-fixed-size on rhel-10 for gh1336
    RHEL issue: https://issues.redhat.com/browse/RHEL-66617

--- tmpfs-fixed_size.sh
+++ tmpfs-fixed_size.sh
@@ -19,7 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="storage"
+TESTTYPE="storage gh1336"
 
 . ${KSTESTDIR}/functions.sh
 

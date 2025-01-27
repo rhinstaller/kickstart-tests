2d0c58d919a9f2b76847d099f6716e13a9ea94c0
Radek Vykydal <rvykydal@redhat.com>
2025-01-27 15:47:29 +0100

    Disable certificate test on rhel9 until the feature is released (rhel61430)
    Related: RHEL-61430

--- certificate.sh
+++ certificate.sh
@@ -19,6 +19,6 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="security"
+TESTTYPE="security rhel61430"
 
 . ${KSTESTDIR}/functions.sh

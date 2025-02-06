1292de4e1cae9b29ec76168438409c2a253c60b4
Radek Vykydal <rvykydal@redhat.com>
2025-02-06 14:24:43 +0100

    Revert "Disable certificate test on rhel9 until the feature is released (rhel61430)"
    This reverts commit 2d0c58d919a9f2b76847d099f6716e13a9ea94c0.

--- certificate.sh
+++ certificate.sh
@@ -19,6 +19,6 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="security rhel61430"
+TESTTYPE="security"
 
 . ${KSTESTDIR}/functions.sh

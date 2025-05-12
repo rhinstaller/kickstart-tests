66fc0cdb5a325682d635dc65256aee64c51db234
Radek Vykydal <rvykydal@redhat.com>
2025-05-12 13:05:25 +0200

    DBG trigger the check

--- raid-ddf.sh
+++ raid-ddf.sh
@@ -21,6 +21,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
+# trigger check
 TESTTYPE="raid storage"
 
 . ${KSTESTDIR}/functions.sh

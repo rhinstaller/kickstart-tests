ca63bb6173383265969d56107f0ba1f058890d21
Radek Vykydal <rvykydal@redhat.com>
2025-06-05 13:39:36 +0200

    Make tests fail if required additional repo is missing (gh1430)
    This would for example prevent false positives caused by testing initial
    setup with anaconda from installation repository (instead of the tested
    anaconda).

--- functions.sh
+++ functions.sh
@@ -443,6 +443,9 @@ export_additional_repo() {
         # Store the server address in file so that it can be sourced later
         # (eq. in kernel_args() and similar).
         echo addrepo_url=${httpd_url} > ${tmpdir}/addrepo_url
+    else
+        >&2 echo "ERROR: Additional repository expected in data/additional_repo is missing"
+        exit 2
     fi
 }
 

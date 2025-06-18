512cfdaff75858c3331ea0c9b69364dc8a146968
Radek Vykydal <rvykydal@redhat.com>
2025-06-18 08:55:00 +0200

    Disable lvm-cache-* on daily-iso temporarily (gh1437)
    The test is failing on daily iso, tracked in gh1437.

--- lvm-cache-2.sh
+++ lvm-cache-2.sh
@@ -19,7 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="lvm storage"
+TESTTYPE="lvm storage gh1437"
 
 . ${KSTESTDIR}/functions.sh
 

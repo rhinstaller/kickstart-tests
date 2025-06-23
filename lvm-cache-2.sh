d884c929b73d064c2cca2b7c1d37e36fe07e43eb
Radek Vykydal <rvykydal@redhat.com>
2025-06-23 10:49:36 +0200

    Revert "Disable lvm-cache-* on daily-iso temporarily (gh1437)"
    This reverts commit 512cfdaff75858c3331ea0c9b69364dc8a146968.

--- lvm-cache-2.sh
+++ lvm-cache-2.sh
@@ -19,7 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="lvm storage gh1437"
+TESTTYPE="lvm storage"
 
 . ${KSTESTDIR}/functions.sh
 

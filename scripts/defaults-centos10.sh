c5ab94d8f15d24a85db21faac5699d2e2fa3802d
Radek Vykydal <rvykydal@redhat.com>
2025-07-21 16:27:04 +0200

    Add centos10 platform to the launch script
    Resolves: INSTALLER-4014

---
+++ scripts/defaults-centos10.sh
@@ -0,0 +1,9 @@
+# Default settings for testing RHEL 10. This requires being inside the Red Hat VPN.
+
+# This is a reasonable default used until the new release is detected by osinfo library.
+export KSTEST_OSINFO_NAME=fedora-eln
+source ./network-device-names.cfg
+export KSTEST_URL='https://mirror.stream.centos.org/10-stream/BaseOS/x86_64/os/'
+export KSTEST_MODULAR_URL='https://mirror.stream.centos.org/10-stream/AppStream/x86_64/os/'
+export KSTEST_FTP_URL='ftp://download.stream.rdu2.redhat.com/stream-10/production/latest-CentOS-Stream/compose/BaseOS/x86_64/os/'
+export KSTEST_OSTREECONTAINER_URL='quay.io/centos-bootc/centos-bootc:stream10'

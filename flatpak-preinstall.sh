3ebfdc1e1d8ba3a568a5e2c9f79652ec0edddff2
Radek Vykydal <rvykydal@redhat.com>
2025-08-04 11:38:56 +0200

    Disable flatpak-preinstall on centos10 (gh1467)
    Disable the failing test until gh1467 is resolved.

--- flatpak-preinstall.sh
+++ flatpak-preinstall.sh
@@ -18,6 +18,6 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="packaging payload skip-on-fedora skip-on-rhel-9"
+TESTTYPE="packaging payload skip-on-fedora skip-on-rhel-9 gh1467"
 
 . ${KSTESTDIR}/functions.sh

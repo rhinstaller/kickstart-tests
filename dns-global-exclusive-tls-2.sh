aeae21ce76372d30a23c1d7e18638f2172f1b25e
Radek Vykydal <rvykydal@redhat.com>
2025-09-01 16:32:49 +0200

    Disable edns tests checking name resolution on fedora until gh1493 is fixed

--- dns-global-exclusive-tls-2.sh
+++ dns-global-exclusive-tls-2.sh
@@ -19,7 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE=${TESTTYPE:-"network dns skip-on-rhel"}
+TESTTYPE=${TESTTYPE:-"network dns skip-on-rhel gh1493"}
 
 . ${KSTESTDIR}/functions.sh
 

cc0ddba115762adad346f261d1397e23f23cf3b0
Radek Vykydal <rvykydal@redhat.com>
2025-09-03 09:55:50 +0200

    Disable rpm-ostree-container-silverblue until gh1496 is fixed

--- rpm-ostree-container-silverblue.sh
+++ rpm-ostree-container-silverblue.sh
@@ -19,7 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="payload ostree skip-on-rhel skip-on-centos"
+TESTTYPE="payload ostree skip-on-rhel skip-on-centos gh1496"
 
 . ${KSTESTDIR}/functions.sh
 

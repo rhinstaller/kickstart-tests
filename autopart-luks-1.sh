b888f374b734cdc466338eac6efe1567d42d4bca
Radek Vykydal <rvykydal@redhat.com>
2025-09-26 09:01:26 +0200

    Reenable autopart-luks-1 (gh774)
    The test flakiness seems to have disappeared.

--- autopart-luks-1.sh
+++ autopart-luks-1.sh
@@ -19,7 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="autopart storage luks gh774"
+TESTTYPE="autopart storage luks"
 
 . ${KSTESTDIR}/functions.sh
 

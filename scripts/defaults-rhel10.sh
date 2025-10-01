de89a53092951df4301433ee880632b40a57e6a8
Radek Vykydal <rvykydal@redhat.com>
2025-10-01 09:20:06 +0200

    daily tests: switch to RHEL 10.2

--- scripts/defaults-rhel10.sh
+++ scripts/defaults-rhel10.sh
@@ -3,8 +3,8 @@
 # This is a reasonable default used until the new release is detected by osinfo library.
 export KSTEST_OSINFO_NAME=fedora-eln
 source ./network-device-names.cfg
-export KSTEST_URL='http://download.devel.redhat.com/rhel-10/nightly/RHEL-10/latest-RHEL-10.1/compose/BaseOS/x86_64/os/'
-export KSTEST_MODULAR_URL='http://download.devel.redhat.com/rhel-10/nightly/RHEL-10/latest-RHEL-10.1/compose/AppStream/x86_64/os/'
-export KSTEST_FTP_URL='ftp://download.devel.redhat.com/mnt/redhat/rhel-10/nightly/RHEL-10/latest-RHEL-10.1/compose/BaseOS/x86_64/os/'
-export KSTEST_FTP_APPSTREAM_URL='ftp://download.devel.redhat.com/mnt/redhat/rhel-10/nightly/RHEL-10/latest-RHEL-10.1/compose/AppStream/x86_64/os/'
-export KSTEST_OSTREECONTAINER_URL='images.paas.redhat.com/bootc/rhel-bootc:latest-10.1'
+export KSTEST_URL='http://download.devel.redhat.com/rhel-10/nightly/RHEL-10/latest-RHEL-10.2/compose/BaseOS/x86_64/os/'
+export KSTEST_MODULAR_URL='http://download.devel.redhat.com/rhel-10/nightly/RHEL-10/latest-RHEL-10.2/compose/AppStream/x86_64/os/'
+export KSTEST_FTP_URL='ftp://download.devel.redhat.com/mnt/redhat/rhel-10/nightly/RHEL-10/latest-RHEL-10.2/compose/BaseOS/x86_64/os/'
+export KSTEST_FTP_APPSTREAM_URL='ftp://download.devel.redhat.com/mnt/redhat/rhel-10/nightly/RHEL-10/latest-RHEL-10.2/compose/AppStream/x86_64/os/'
+export KSTEST_OSTREECONTAINER_URL='images.paas.redhat.com/bootc/rhel-bootc:latest-10.2'

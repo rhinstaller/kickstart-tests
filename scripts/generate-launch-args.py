rhinstaller/kickstart-tests

commit aae9d5103e21cdb8138dc97069da4323af328fb5

Support --testtypes in arguments and query generator scripts
Related: INSTALLER-4293

scripts/generate-launch-args.py
@@ -15,13 +15,22 @@ OS_VARIANT_TO_PLATFORM = {
     'centos10': 'centos10',
 }
 
+OS_VARIANT_TO_SKIPPED = {
+    'daily-iso': 'SKIPPED_TESTTYPES_DAILY_ISO',
+    'rawhide': 'SKIPPED_TESTTYPES_RAWHIDE',
+    'rhel8': 'SKIPPED_TESTTYPES_RHEL8',
+    'rhel9': 'SKIPPED_TESTTYPES_RHEL9',
+    'rhel10': 'SKIPPED_TESTTYPES_RHEL10',
+    'centos10': 'SKIPPED_TESTTYPES_CENTOS10',
+}
+
 OS_VARIANT_TO_DISABLED = {
-    'daily-iso': 'SKIP_TESTTYPES_DAILY_ISO',
-    'rawhide': 'SKIP_TESTTYPES_RAWHIDE',
-    'rhel8': 'SKIP_TESTTYPES_RHEL8',
-    'rhel9': 'SKIP_TESTTYPES_RHEL9',
-    'rhel10': 'SKIP_TESTTYPES_RHEL10',
-    'centos10': 'SKIP_TESTTYPES_CENTOS10',
+    'daily-iso': 'DISABLED_TESTTYPES_DAILY_ISO',
+    'rawhide': 'DISABLED_TESTTYPES_RAWHIDE',
+    'rhel8': 'DISABLED_TESTTYPES_RHEL8',
+    'rhel9': 'DISABLED_TESTTYPES_RHEL9',
+    'rhel10': 'DISABLED_TESTTYPES_RHEL10',
+    'centos10': 'DISABLED_TESTTYPES_CENTOS10',
 }
 
 RE_MASTER = re.compile('^main$')
@@ -44,30 +53,32 @@ def get_skip_testtypes(skip_file, variable):
 def get_arguments_for_branch(branch, skip_file):
     platform = None
     skip_testtypes = []
+    disabled_testtypes = []
 
     if RE_MASTER.match(branch):
         platform = "fedora_rawhide"
-        skipvar = 'SKIP_TESTTYPES_RAWHIDE'
+        os_variant = "rawhide"
     elif RE_FEDORA.match(branch):
         platform = "fedora_rawhide"
-        skipvar = 'SKIP_TESTTYPES_RAWHIDE'
+        os_variant = "rawhide"
     elif RE_RHEL8.match(branch):
         platform = "rhel8"
-        skipvar = 'SKIP_TESTTYPES_RHEL8'
+        os_variant = "rhel8"
     elif RE_RHEL9.match(branch):
         platform = "rhel9"
-        skipvar = 'SKIP_TESTTYPES_RHEL9'
+        os_variant = "rhel9"
     elif RE_RHEL10.match(branch):
         platform = "rhel10"
-        skipvar = 'SKIP_TESTTYPES_RHEL10'
+        os_variant = "rhel10"
     else:
         platform = None
-        skipvar = None
+        os_variant = None
 
-    if skipvar:
-        skip_testtypes = get_skip_testtypes(skip_file, skipvar)
+    if platform:
+        skip_testtypes = get_skip_testtypes(skip_file, OS_VARIANT_TO_SKIPPED[os_variant])
+        disabled_testtypes = get_skip_testtypes(skip_file, OS_VARIANT_TO_DISABLED[os_variant])
 
-    return (platform, skip_testtypes)
+    return (platform, skip_testtypes, disabled_testtypes)
 
 
 def parse_args():
@@ -77,6 +88,8 @@ def parse_args():
     _parser.add_argument("--skip-testtypes", "-s", type=str, metavar="TYPE[,TYPE..]",
                          help="skip tests with TYPE (tag)")
     _parser.add_argument("--testtype", "-t", type=str, metavar="TYPE",
+                         help="only run tests with TYPE (tag)")
+    _parser.add_argument("--testtypes", "-T", type=str, metavar="TYPE[,TYPE..]",
                          help="only run tests with TYPE (tag)")
     _parser.add_argument("tests", nargs='*', metavar="TESTNAME",
                          help="names of test to be run")
@@ -91,6 +104,8 @@ def parse_args():
                          help="file containing data about disabled tests")
     _parser.add_argument("--anaconda-pr", "-p", action="store_true",
                          help="skip tests not working on anaconda PR")
+    _parser.add_argument("--disabled", "-d", action="store_true",
+                         help="run disabled tests")
     return _parser.parse_args()
 
 
@@ -104,19 +119,22 @@ if __name__ == "__main__":
     platform_args = []
     testtype_args = []
     skip_testtypes_args = []
+    testtypes_args = []
 
     platform = None
+    skipped_testtypes = []
     disabled_testtypes = []
     if args.os_variant:
         platform = OS_VARIANT_TO_PLATFORM[args.os_variant]
+        skipped_testtypes = get_skip_testtypes(skip_file, OS_VARIANT_TO_SKIPPED[args.os_variant])
         disabled_testtypes = get_skip_testtypes(skip_file, OS_VARIANT_TO_DISABLED[args.os_variant])
     elif args.branch:
-        platform, disabled_testtypes = get_arguments_for_branch(args.branch, skip_file)
+        platform, skipped_testtypes, disabled_testtypes = get_arguments_for_branch(args.branch, skip_file)
         if not platform:
             raise ValueError("Platform for branch {} is not defined".format(args.branch))
 
     if args.anaconda_pr:
-        disabled_testtypes.extend(get_skip_testtypes(skip_file, "SKIP_TESTTYPES_ANACONDA_PR"))
+        skipped_testtypes.extend(get_skip_testtypes(skip_file, "SKIPPED_TESTTYPES_ANACONDA_PR"))
 
     if platform:
         platform_args = ["--platform", platform]
@@ -124,13 +142,21 @@ if __name__ == "__main__":
     if args.testtype:
         testtype_args = ["--testtype", args.testtype]
 
-    skip_testtypes = [] if args.force else disabled_testtypes
+    testtypes = disabled_testtypes if args.disabled else []
+    if args.testtypes:
+        testtypes.extend(args.testtypes.split(','))
+    if testtypes:
+        testtypes_args = ["--testtypes", ",".join(testtypes)]
+
+    skip_testtypes = [] if args.force else skipped_testtypes
+    if not args.disabled:
+        skip_testtypes.extend(disabled_testtypes)
     if args.skip_testtypes:
         skip_testtypes.extend(args.skip_testtypes.split(','))
     if skip_testtypes:
         skip_testtypes_args = ["--skip-testtypes", ",".join(skip_testtypes)]
 
-    launch_args = platform_args + skip_testtypes_args + testtype_args + args.tests
+    launch_args = platform_args + skip_testtypes_args + testtype_args + args.tests + testtypes_args
 
     print(
         " ".join(launch_args)

.github/workflows/scenarios-permian.yml
@@ -51,22 +51,12 @@ jobs:
           set -eux
           TESTPLAN="./testlib/test_plans/daily-${{ matrix.scenario }}.plan.yaml"
           TEMPLATE="${TESTPLAN}.j2"
-          if [ "${{ matrix.scenario }}" == "daily-iso" ]; then
-            VARIABLE="SKIP_TESTTYPES_DAILY_ISO"
-          elif [ "${{ matrix.scenario }}" == "rhel8" ]; then
-            VARIABLE="SKIP_TESTTYPES_RHEL8"
-          elif [ "${{ matrix.scenario }}" == "rhel9" ]; then
-            VARIABLE="SKIP_TESTTYPES_RHEL9"
-          elif [ "${{ matrix.scenario }}" == "rhel10" ]; then
-            VARIABLE="SKIP_TESTTYPES_RHEL10"
-          elif [ "${{ matrix.scenario }}" == "centos10" ]; then
-            VARIABLE="SKIP_TESTTYPES_CENTOS10"
-          fi
-          if [ -e ${TEMPLATE} ] && [ -n ${VARIABLE} ]; then
+          OS_VARIANT="${{ matrix.scenario }}"
+          if [ -e ${TEMPLATE} ] && [ -n ${OS_VARIANT} ]; then
             scripts/generate-testplan.py \
               -t ${TEMPLATE} \
               -f ./containers/runner/skip-testtypes \
-              -s ${VARIABLE} \
+              -r ${OS_VARIANT} \
               -o ${TESTPLAN} \
               --verbose
           fi

containers/runner/skip-testtypes
@@ -10,6 +10,9 @@ common_skip_array=(
 
 fedora_skip_array=(
   skip-on-fedora
+)
+
+fedora_disabled_array=(
   gh576       # clearpart-4 test is flaky on all scenarios
   gh595       # proxy-cmdline failing on all scenarios
   gh641       # packages-multilib failing on systemd conflict
@@ -25,21 +28,23 @@ fedora_skip_array=(
   gh1493      # dns-global-exclusive-tls-* failing
 )
 
-daily_iso_skip_array=(
+daily_iso_disabled_array=(
   gh1434      # reboot-inital-setup-* failing
 )
 
-rawhide_skip_array=(
+rawhide_disabled_array=(
 )
 
-rawhide_text_skip_array=(
-  skip-on-fedora
+rawhide_text_disabled_array=(
   rhbz1853668 # multipath device not constructed back after installation
 )
 
 rhel8_skip_array=(
   skip-on-rhel
   skip-on-rhel-8
+)
+
+rhel8_disabled_array=(
   gh576       # clearpart-4 test is flaky on all scenarios
   gh595       # proxy-cmdline failing on all scenarios
   gh774       # autopart-luks-1 failing
@@ -50,6 +55,9 @@ rhel8_skip_array=(
 rhel9_skip_array=(
   skip-on-rhel
   skip-on-rhel-9
+)
+
+rhel9_disabled_array=(
   gh576       # clearpart-4 test is flaky on all scenarios
   gh595       # proxy-cmdline failing on all scenarios
   gh604       # packages-weakdeps: "gnupg2 --recommends has changed, test needs to be updated"
@@ -58,7 +66,7 @@ rhel9_skip_array=(
   gh804       # tests requiring dvd iso failing
 )
 
-rhel10_centos10_skip_array=(
+rhel10_centos10_disabled_array=(
   gh576       # clearpart-4 test is flaky on all scenarios
   gh804       # tests requiring dvd iso failing
   gh1090      # raid-1-reqpart failing
@@ -74,6 +82,9 @@ rhel10_skip_array=(
 centos10_skip_array=(
   skip-on-centos
   skip-on-centos-10
+)
+
+centos10_disabled_array=(
   gh1466      # ftp source tests failing
   gh1474      # proxy-cmdline failing
 )
@@ -94,13 +105,23 @@ _join_args_by_comma(){
 }
 
 # Do not forget to add new releases below as well
-SKIP_TESTTYPES_RAWHIDE=$(_join_args_by_comma "${common_skip_array[@]}" "${fedora_skip_array[@]}" "${rawhide_skip_array[@]}")
-SKIP_TESTTYPES_RAWHIDE_TEXT=$(_join_args_by_comma "${common_skip_array[@]}" "${rawhide_text_skip_array[@]}")
-SKIP_TESTTYPES_DAILY_ISO=$(_join_args_by_comma "${common_skip_array[@]}" "${fedora_skip_array[@]}" "${daily_iso_skip_array[@]}")
-SKIP_TESTTYPES_RHEL8=$(_join_args_by_comma "${common_skip_array[@]}" "${rhel8_skip_array[@]}")
-SKIP_TESTTYPES_RHEL9=$(_join_args_by_comma "${common_skip_array[@]}" "${rhel9_skip_array[@]}")
-SKIP_TESTTYPES_RHEL10=$(_join_args_by_comma "${common_skip_array[@]}" "${rhel10_centos10_skip_array[@]}" "${rhel10_skip_array[@]}")
-SKIP_TESTTYPES_CENTOS10=$(_join_args_by_comma "${common_skip_array[@]}" "${rhel10_centos10_skip_array[@]}" "${centos10_skip_array[@]}")
-SKIP_TESTTYPES_RHEL8_DAILY=$(_join_args_by_comma "${common_skip_array[@]}" "${rhel8_daily_skip_array[@]}")
+SKIPPED_TESTTYPES_RAWHIDE=$(_join_args_by_comma "${common_skip_array[@]}" "${fedora_skip_array[@]}")
+SKIPPED_TESTTYPES_RAWHIDE_TEXT=$(_join_args_by_comma "${common_skip_array[@]}" "${fedora_skip_array[@]}")
+SKIPPED_TESTTYPES_DAILY_ISO=$(_join_args_by_comma "${common_skip_array[@]}" "${fedora_skip_array[@]}")
+SKIPPED_TESTTYPES_RHEL8=$(_join_args_by_comma "${common_skip_array[@]}" "${rhel8_skip_array[@]}")
+SKIPPED_TESTTYPES_RHEL9=$(_join_args_by_comma "${common_skip_array[@]}" "${rhel9_skip_array[@]}")
+SKIPPED_TESTTYPES_RHEL10=$(_join_args_by_comma "${common_skip_array[@]}" "${rhel10_skip_array[@]}")
+SKIPPED_TESTTYPES_CENTOS10=$(_join_args_by_comma "${common_skip_array[@]}" "${centos10_skip_array[@]}")
+SKIPPED_TESTTYPES_RHEL8_DAILY=$(_join_args_by_comma "${common_skip_array[@]}" "${rhel8_daily_skip_array[@]}")
 # Tests run on an anaconda pull request by comment
-SKIP_TESTTYPES_ANACONDA_PR=$(_join_args_by_comma "${common_skip_array[@]}" "${anaconda_pr_skip_array[@]}")
+SKIPPED_TESTTYPES_ANACONDA_PR=$(_join_args_by_comma "${common_skip_array[@]}" "${anaconda_pr_skip_array[@]}")
+
+DISABLED_TESTTYPES_DAILY_ISO=$(_join_args_by_comma "${fedora_disabled_array[@]}" "${daily_iso_disabled_array[@]}")
+DISABLED_TESTTYPES_RAWHIDE=$(_join_args_by_comma "${fedora_disabled_array[@]}" "${rawhide_disabled_array[@]}")
+DISABLED_TESTTYPES_RHEL8=$(_join_args_by_comma "${rhel8_disabled_array[@]}")
+DISABLED_TESTTYPES_RHEL9=$(_join_args_by_comma "${rhel9_disabled_array[@]}")
+DISABLED_TESTTYPES_RHEL10=$(_join_args_by_comma "${rhel10_centos10_disabled_array[@]}")
+DISABLED_TESTTYPES_CENTOS10=$(_join_args_by_comma "${rhel10_centos10_disabled_array[@]}" "${centos10_disabled_array[@]}")
+
+# Keeping for scripts/run-ci.sh
+SKIP_TESTTYPES_RAWHIDE="${SKIPPED_TESTTYPES_RAWHIDE},${DISABLED_TESTTYPES_RAWHIDE}"

scripts/generate-permian-query.py
@@ -13,6 +13,8 @@ def parse_args():
                          nargs=1, action="append", help="skip tests with TYPE (tag)")
     _parser.add_argument("--testtype", "-t", type=str, metavar="TYPE",
                          help="only run tests with TYPE (tag)")
+    _parser.add_argument("--testtypes", "-T", type=str, metavar="TYPE[,TYPE..]",
+                         nargs=1, action="append", help="only run tests with TYPE (tag)")
     _parser.add_argument("--platform", "-p", type=str, metavar="PLATFORM", default="",
                          help="platform to be used for tests")
     _parser.add_argument("--print-platform", action="store_true",
@@ -49,6 +51,11 @@ if __name__ == "__main__":
         if args.skip_testtypes:
             skiptypes = ','.join(itertools.chain(*args.skip_testtypes)).split(',')
             conditions.extend(['"{}" not in tc.tags'.format(skiptype) for skiptype in skiptypes])
+        if args.testtypes:
+            testtypes = ','.join(itertools.chain(*args.testtypes)).split(',')
+            testtype_conditions = ['"{}" in tc.tags'.format(testtype) for testtype in testtypes]
+            testtypes_condition = "( " + " or ".join(testtype_conditions) + " )"
+            conditions.append(testtypes_condition)
 
     if not conditions:
         query = "True"

scripts/generate-testplan.py
@@ -22,6 +22,24 @@ import argparse
 import subprocess
 from jinja2 import Template
 
+OS_VARIANT_TO_SKIPPED = {
+    'daily-iso': 'SKIPPED_TESTTYPES_DAILY_ISO',
+    'rawhide': 'SKIPPED_TESTTYPES_RAWHIDE',
+    'rhel8': 'SKIPPED_TESTTYPES_RHEL8',
+    'rhel9': 'SKIPPED_TESTTYPES_RHEL9',
+    'rhel10': 'SKIPPED_TESTTYPES_RHEL10',
+    'centos10': 'SKIPPED_TESTTYPES_CENTOS10',
+}
+
+OS_VARIANT_TO_DISABLED = {
+    'daily-iso': 'DISABLED_TESTTYPES_DAILY_ISO',
+    'rawhide': 'DISABLED_TESTTYPES_RAWHIDE',
+    'rhel8': 'DISABLED_TESTTYPES_RHEL8',
+    'rhel9': 'DISABLED_TESTTYPES_RHEL9',
+    'rhel10': 'DISABLED_TESTTYPES_RHEL10',
+    'centos10': 'DISABLED_TESTTYPES_CENTOS10',
+}
+
 
 def parse_args():
     _parser = argparse.ArgumentParser(description="Generate testplan for scenario from template and skip-test tags")
@@ -31,9 +49,8 @@ def parse_args():
     _parser.add_argument("--skiptest-file", "-f", type=str, required=True,
                          metavar="SKIP_TEST_FILE",
                          help="File with skip test variables")
-    _parser.add_argument("--skiptest-variable", "-s", type=str, required=True,
-                         metavar="SKIP_TEST_VARIABLE",
-                         help="Variable from the file to be used.")
+    _parser.add_argument("--os-variant", "-r", type=str, metavar="OS_VARIANT",
+                         help="os variant of the boot.iso to be tested (for example rhel9)")
     _parser.add_argument("--output", "-o", type=str,
                          metavar="OUTPUT_FILE",
                          help="Output file.")
@@ -53,14 +70,17 @@ if __name__ == "__main__":
 
     args = parse_args()
 
-    var_value = get_variable_from_shell_file(args.skiptest_variable, args.skiptest_file)
-    tag_list = var_value.split(',')
+    skipped_testtypes = get_variable_from_shell_file(OS_VARIANT_TO_SKIPPED[args.os_variant],
+                                                     args.skiptest_file)
+    disabled_testtypes = get_variable_from_shell_file(OS_VARIANT_TO_DISABLED[args.os_variant],
+                                                      args.skiptest_file)
+    tag_list = skipped_testtypes.split(',') + disabled_testtypes.split(',')
 
     with open(args.template_file, 'r') as f:
         template_tp = f.read()
 
     template = Template(template_tp, trim_blocks=True)
-    header = f"# Query generated from {args.skiptest_variable} of {args.skiptest_file}"
+    header = f"# Query generated for {args.os_variant} from {args.skiptest_file}"
     tp = template.render(skiptags=tag_list)
 
     if args.output:

commit 3ac13cb5a92d06042f686073978da0f35ca09be7

Fix --force option of generate-launch-args script
The option was broken by commit aae9d5103e21cdb8138dc97069da4323af328fb5
which made it skipping disabled tests.

Related: INSTALLER-4293

scripts/generate-launch-args.py
@@ -151,9 +151,11 @@ if __name__ == "__main__":
     if testtypes:
         testtypes_args = ["--testtypes", ",".join(testtypes)]
 
-    skip_testtypes = [] if args.force else skipped_testtypes
+    skip_testtypes = skipped_testtypes
     if not args.disabled:
         skip_testtypes.extend(disabled_testtypes)
+    if args.force:
+        skip_testtypes = []
     if args.skip_testtypes:
         skip_testtypes.extend(args.skip_testtypes.split(','))
     if skip_testtypes:
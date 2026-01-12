294804d247a61c6b97ee7ea1cabe35e097437ba8
Katerina Koukiou <k.koukiou@gmail.com>
2026-01-12 15:38:01 +0100

    Enable bootc tests on rhel-10

--- bootc-basic.sh
+++ bootc-basic.sh
@@ -20,7 +20,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="skip-on-rhel-9 payload bootc rhel58215 gh1574"
+TESTTYPE="skip-on-rhel-9 payload bootc gh1574"
 
 . ${KSTESTDIR}/functions.sh
 

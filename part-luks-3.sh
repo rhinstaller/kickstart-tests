3d206bed4d00ad227d8e08fcb5cd195827c69de7
Katerina Koukiou <k.koukiou@gmail.com>
2026-03-30 08:05:54 +0200

    Disable LUKS tests on RHEL / CentOS (gh#1637)
    Tag scripts that use copy_file_encrypted with gh1637 and add gh1637 to
    rhel10_centos10 disabled test types in containers/runner/skip-testtypes.
    
    Guestfish -i / inspect fails on RHEL crypttab with link-volume-key=...
    (rhbz#2452937).
    
    Tracked in https://github.com/rhinstaller/kickstart-tests/issues/1637

--- part-luks-3.sh
+++ part-luks-3.sh
@@ -19,7 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="storage partition luks"
+TESTTYPE="storage partition luks gh1637"
 
 . ${KSTESTDIR}/functions.sh
 

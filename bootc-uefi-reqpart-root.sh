9be0d50be407e983cca97ffb6d0a6c1370dcd7be
Katerina Koukiou <k.koukiou@gmail.com>
2026-07-06 08:21:44 +0200

    Add bootc UEFI kickstart test without separate /boot partition
    Test bootc installation with reqpart and a single root partition on
    UEFI, where /boot lives on / and only /boot/efi is mounted separately.
    
    Related: RHEL-187037

---
+++ bootc-uefi-reqpart-root.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# Copyright (C) 2026  Red Hat, Inc.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions of
+# the GNU General Public License v.2, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY expressed or implied, including the implied warranties of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.  You should have received a copy of the
+# GNU General Public License along with this program; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.  Any Red Hat trademarks that are incorporated in the
+# source code or documentation are not subject to the GNU General Public
+# License and may only be used or replicated with the express permission of
+# Red Hat, Inc.
+#
+
+# Ignore unused variable parsed out by tooling scripts as test tags metadata
+# shellcheck disable=SC2034
+TESTTYPE="skip-on-rhel-9 payload uefi bootc reboot storage gh1574"
+
+. ${KSTESTDIR}/functions.sh
+
+enable_uefi() {
+    echo "true"
+}
+
+copy_interesting_files_from_system() {
+    local disksdir
+    disksdir="${1}"
+
+    # Find disks.
+    local args
+    args=$(for d in ${disksdir}/disk-*img; do echo -a ${d}; done)
+
+    # Use also iscsi disk if there is any.
+    if [[ -n ${iscsi_disk_img} ]]; then
+        args="${args} -a ${disksdir}/${iscsi_disk_img}"
+    fi
+
+    # Grab files out of the installed system while it still exists.
+    # Grab these files:
+    #
+    # logs from Anaconda - whole /var/log/anaconda/ directory is copied out,
+    #                      this can be used for saving specific test output
+    # original-ks.cfg - the kickstart used for the test
+    # anaconda-ks.cfg - the kickstart saved after installation, useful for
+    #                   debugging
+    # RESULT - file from the test
+    #
+    # The location of aforementioned files is different in an ostree system
+
+    # Find root device by filesystem label from reqpart_root.ks
+    root_device="$(guestfish ${args} <<< "
+        launch
+        findfs-label rootfs
+        " 2>/dev/null)"
+    path_prefix="/ostree/deploy/test-stateroot"
+
+    if [ -z "${root_device}" ]; then
+        echo "Couldn't find root filesystem with label rootfs"
+        return 1
+    fi
+
+    for item in ${path_prefix}/var/roothome/original-ks.cfg \
+                ${path_prefix}/var/roothome/anaconda-ks.cfg \
+                ${path_prefix}/var/roothome/anabot.log \
+                ${path_prefix}/var/log/anaconda/ \
+                ${path_prefix}/var/roothome/RESULT
+    do
+        guestfish ${args} <<< "
+            launch
+            mount ${root_device} /
+            copy-out '${item}' '${disksdir}'
+            "
+    done
+}
+
+additional_runner_args() {
+   # Wait for reboot and shutdown of the VM,
+   # but exit after the specified timeout.
+   echo "--wait $(get_timeout)"
+}

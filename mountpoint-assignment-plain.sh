74788a23c055a4f93754f058424fac10262bdfc4
Radek Vykydal <rvykydal@redhat.com>
2026-07-16 11:00:42 +0200

    Migrate mountpoint-assignment-plain to EFI boot partition.
    Resolves: INSTALLER-4817

--- mountpoint-assignment-plain.sh
+++ mountpoint-assignment-plain.sh
@@ -19,10 +19,7 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="mount storage biosboot"
+TESTTYPE="mount storage"
 
 . ${KSTESTDIR}/functions.sh
 
-enable_uefi() {
-    echo "false"
-}

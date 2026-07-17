a26e8ed241fbef9dffb34feff89a05c67e0b2cb0
Radek Vykydal <rvykydal@redhat.com>
2026-07-17 09:30:23 +0200

    Migrate snapshot-pre test to UEFI.
    Resolves: INSTALLER-4820
    
    It seems to be working well with UEFI

--- snapshot-pre.sh
+++ snapshot-pre.sh
@@ -19,10 +19,6 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="snapshot lvm storage biosboot"
+TESTTYPE="snapshot lvm storage"
 
 . ${KSTESTDIR}/functions.sh
-
-enable_uefi() {
-    echo "false"
-}

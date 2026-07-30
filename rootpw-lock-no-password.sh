5897b898666b7b7e88e917aacacab589a0eac9b5
Katerina Koukiou <k.koukiou@gmail.com>
2026-07-30 13:08:31 +0200

    Disable tests that lock root account with different issue
    They were incorrectly marked.

--- rootpw-lock-no-password.sh
+++ rootpw-lock-no-password.sh
@@ -19,6 +19,6 @@
 
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
-TESTTYPE="users gh1716"
+TESTTYPE="users gh1727"
 
 . ${KSTESTDIR}/functions.sh

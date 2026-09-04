a5c6a622318d744da2326c776c2fa39aeeec939e
Radek Vykydal <rvykydal@redhat.com>
2026-09-04 10:06:53 +0200

    Modify a test

--- container.sh
+++ container.sh
@@ -17,6 +17,8 @@
 #
 # Red Hat Author(s): Chris Lumens <clumens@redhat.com>
 
+# modify a test
+
 # Ignore unused variable parsed out by tooling scripts as test tags metadata
 # shellcheck disable=SC2034
 TESTTYPE="bootloader packaging payload coverage skip-on-rhel skip-on-centos skip-on-fedora-eln"
